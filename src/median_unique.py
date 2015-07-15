a2081d38d1207c02eaabcd603f0118c5f858e59a
Yi Zhou <zhouyi@berkeley.edu>
2015-07-14 21:54:53 -0700

    improved algorithm and runtime

--- src/median_unique.py
+++ src/median_unique.py
@@ -1,6 +1,7 @@
 # Script that calculates the median number of unique words per tweet.
 _author_ = 'Yi'
 
+import bisect
 import numpy
 import sys
 
@@ -8,14 +9,27 @@ cmd_args = sys.argv
 input_file = cmd_args[1]
 output_file = cmd_args[2]
 
+median_index = 0
+is_odd = 0
+tweet_lengths = []
+
 with open(output_file,'w') as feature_two:
     with open(input_file) as file_tweets:
-        tweet_count = 0
-        tweet_lengths = []
+
         for tweet in file_tweets:
-            tweet_count += 1
+            if is_odd == 0:
+                is_odd = 1
+            else:
+                is_odd = 0
+
             tweet_by_word = tweet.split(' ')
             tweet_length = len(tweet_by_word)
-            tweet_lengths.append(tweet_length)
-            moving_median = numpy.median(tweet_lengths)
-            feature_two.write(str(moving_median) + '\n')
+            bisect.insort_left(tweet_lengths,tweet_length)
+
+            if is_odd == 1:
+                moving_median = tweet_lengths[median_index]
+            else:
+                moving_median = numpy.median([tweet_lengths[median_index], tweet_lengths[median_index+1]])
+                median_index += 1
+
+            feature_two.write('%.1f\n' % moving_median)
--- src/words_tweeted.py
+++ src/words_tweeted.py
@@ -1,24 +1,28 @@
 # Script that calculates the total number of times each word has been tweeted.
 _author_ = 'Yi'
 
+from itertools import groupby
+from operator import itemgetter
 import sys
 
 cmd_args = sys.argv
 input_file = cmd_args[1]
 output_file = cmd_args[2]
 
-word_count = {}
+word_counts = []
 
 with open(input_file) as file_tweets:
     for tweet in file_tweets:
+
         tweet_stripped = tweet.strip() # remove '\n'
         tweet_by_word = tweet_stripped.split(' ')
+
         for word in tweet_by_word:
-            if word in word_count.keys():
-                word_count[word] += 1
-            else:
-                word_count[word] = 1
+            word_counts.append([word,1])
+
+word_counts.sort()
 
 with open(output_file,'w') as feature_one:
-    for word in sorted(word_count):
-        feature_one.write(word + ' ' + str(word_count[word]) + '\n')
+    for this_word, word_group in groupby(word_counts, itemgetter(0)):
+        word_total_count = sum(word_count for this_word, word_count in word_group)
+        feature_one.write(this_word + '\t' + str(word_total_count) + '\n')
